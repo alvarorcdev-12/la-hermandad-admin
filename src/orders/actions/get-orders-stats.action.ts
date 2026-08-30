ea13f0d1f5b6d83697d7fbe8125beb828d645550
Alvaro Ruiz Catoira <alvarorcdev@gmail.com>
2026-08-30 18:32:31 -0400

    feat: add Skeleton component and implement loading states in OrderStats

--- src/orders/actions/get-orders-stats.action.ts
+++ src/orders/actions/get-orders-stats.action.ts
@@ -1,10 +1,13 @@
 import { laHermandadApi } from "@/api/la-hermandad-api";
 import type { OrdersStats } from "../interfaces/orders-stats.interface";
+import { sleep } from "@/lib/sleep";
 
 export const getOrdersStatsAction = async (
   startDate?: string,
   endDate?: string,
 ) => {
+  await sleep(1500);
+
   const { data } = await laHermandadApi.get<OrdersStats>("/orders/stats", {
     params: {
       startDate,
--- src/orders/components/OrderStats.tsx
+++ src/orders/components/OrderStats.tsx
@@ -8,21 +8,25 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { Formatter } from "@/lib/formatter";
+import { getRangeLabel } from "@/lib/get-range-label";
 
 import type { DateRange } from "react-day-picker";
 import type { OrdersStats } from "../interfaces/orders-stats.interface";
-import { Formatter } from "@/lib/formatter";
-import { getRangeLabel } from "@/lib/get-range-label";
 
 interface Props {
   stats?: OrdersStats;
   isLoading: boolean;
 
-  date: DateRange;
-  onDateChange: (date: DateRange) => void;
+  date?: DateRange;
+  onDateChange: (date: DateRange | undefined) => void;
 }
 
 export const OrderStats = ({ date, stats, isLoading, onDateChange }: Props) => {
+  const showSkeleton = isLoading || !stats;
+
   return (
     <Card className="p-0">
       <CardContent className="p-0">
@@ -52,40 +56,85 @@ export const OrderStats = ({ date, stats, isLoading, onDateChange }: Props) => {
             </Popover>
           </div>
           <div className="border-r border-l p-4 w-full">
-            <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
-              Pedidos
-            </p>
-            <p className="font-medium text-sm mt-2">{stats?.orders}</p>
+            {showSkeleton ? (
+              <>
+                <Skeleton className="h-4 w-16" />
+                <Skeleton className="h-5 w-12 mt-2" />
+              </>
+            ) : (
+              <>
+                <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
+                  Pedidos
+                </p>
+                <p className="font-medium text-sm mt-2">{stats.orders}</p>
+              </>
+            )}
           </div>
           <div className="border-r p-4 w-full">
-            <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
-              Productos vendidos
-            </p>
-            <p className="font-medium text-sm mt-2">{stats?.items || 0}</p>
+            {showSkeleton ? (
+              <>
+                <Skeleton className="h-4 w-28" />
+                <Skeleton className="h-5 w-12 mt-2" />
+              </>
+            ) : (
+              <>
+                <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
+                  Productos vendidos
+                </p>
+                <p className="font-medium text-sm mt-2">{stats.items || 0}</p>
+              </>
+            )}
           </div>
           <div className="border-r p-4 w-full">
-            <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
-              Ventas
-            </p>
-            <p className="font-medium text-sm mt-2">
-              {Formatter.currency(stats?.sales || "0")}
-            </p>
+            {showSkeleton ? (
+              <>
+                <Skeleton className="h-4 w-14" />
+                <Skeleton className="h-5 w-20 mt-2" />
+              </>
+            ) : (
+              <>
+                <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
+                  Ventas
+                </p>
+                <p className="font-medium text-sm mt-2">
+                  {Formatter.currency(stats.sales || "0")}
+                </p>
+              </>
+            )}
           </div>
           <div className="border-r p-4 w-full">
-            <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
-              Completados
-            </p>
-            <p className="font-medium text-sm mt-2">
-              {stats?.closedOrders || 0}
-            </p>
+            {showSkeleton ? (
+              <>
+                <Skeleton className="h-4 w-20" />
+                <Skeleton className="h-5 w-12 mt-2" />
+              </>
+            ) : (
+              <>
+                <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
+                  Completados
+                </p>
+                <p className="font-medium text-sm mt-2">
+                  {stats.closedOrders || 0}
+                </p>
+              </>
+            )}
           </div>
           <div className="p-4 w-full">
-            <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
-              Cancelados
-            </p>
-            <p className="font-medium text-sm mt-2">
-              {stats?.cancelledOrders || 0}
-            </p>
+            {showSkeleton ? (
+              <>
+                <Skeleton className="h-4 w-20" />
+                <Skeleton className="h-5 w-12 mt-2" />
+              </>
+            ) : (
+              <>
+                <p className="font-medium text-sm underline decoration-dashed underline-offset-4">
+                  Cancelados
+                </p>
+                <p className="font-medium text-sm mt-2">
+                  {stats.cancelledOrders || 0}
+                </p>
+              </>
+            )}
           </div>
         </div>
       </CardContent>
